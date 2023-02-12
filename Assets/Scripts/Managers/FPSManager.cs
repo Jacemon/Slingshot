33afa5738036aadc4399ed3c9498160b27bc30cd
Jacemon <evgeeeha00@mail.ru>
2023-02-12 13:49:14 +0300

    fps fix

--- Assets/Scripts/Managers/FPSManager.cs
+++ Assets/Scripts/Managers/FPSManager.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 public class FpsManager : MonoBehaviour
 {
     public TextMeshProUGUI fpsLabel;
-    
-    private void Update () {
-        int fps = (int)(1.0f/Time.deltaTime);
+
+    private void Start()
+    {
+        Application.targetFrameRate = int.MaxValue;
+        QualitySettings.vSyncCount = 0;
+    }
+
+    private void Update()
+    {
+        var fps = (int)(1.0f/Time.deltaTime);
         fpsLabel.text = fps.ToString();
     }
 }
