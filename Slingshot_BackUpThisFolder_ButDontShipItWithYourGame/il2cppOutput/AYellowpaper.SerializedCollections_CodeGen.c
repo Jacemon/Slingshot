72965d17f7b7c0fe554430d27a5712bb5e3cb872
Jacemon <evgeeeha00@mail.ru>
2023-06-08 16:56:26 +0300

    keystore

---
+++ Slingshot_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/AYellowpaper.SerializedCollections_CodeGen.c
@@ -0,0 +1,261 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Collections.IEnumerable AYellowpaper.SerializedCollections.DictionaryLookupTable`2::get_Keys()
+// 0x00000002 System.Void AYellowpaper.SerializedCollections.DictionaryLookupTable`2::.ctor(AYellowpaper.SerializedCollections.SerializedDictionary`2<TKey,TValue>)
+// 0x00000003 System.Collections.Generic.IReadOnlyList`1<System.Int32> AYellowpaper.SerializedCollections.DictionaryLookupTable`2::GetOccurences(System.Object)
+// 0x00000004 System.Void AYellowpaper.SerializedCollections.DictionaryLookupTable`2::RecalculateOccurences()
+// 0x00000005 System.Void AYellowpaper.SerializedCollections.DictionaryLookupTable`2::RemoveKey(System.Object)
+// 0x00000006 System.Void AYellowpaper.SerializedCollections.DictionaryLookupTable`2::RemoveAt(System.Int32)
+// 0x00000007 System.Object AYellowpaper.SerializedCollections.DictionaryLookupTable`2::GetKeyAt(System.Int32)
+// 0x00000008 System.Void AYellowpaper.SerializedCollections.DictionaryLookupTable`2::RemoveDuplicates()
+// 0x00000009 System.Void AYellowpaper.SerializedCollections.DictionaryLookupTable`2::AddKey(System.Object)
+// 0x0000000A System.Void AYellowpaper.SerializedCollections.DictionaryLookupTable`2::.cctor()
+// 0x0000000B System.Void AYellowpaper.SerializedCollections.DictionaryLookupTable`2/<>c::.cctor()
+// 0x0000000C System.Void AYellowpaper.SerializedCollections.DictionaryLookupTable`2/<>c::.ctor()
+// 0x0000000D TKey AYellowpaper.SerializedCollections.DictionaryLookupTable`2/<>c::<RemoveDuplicates>b__11_0(AYellowpaper.SerializedCollections.SerializedKeyValuePair`2<TKey,TValue>)
+// 0x0000000E System.Boolean AYellowpaper.SerializedCollections.DictionaryLookupTable`2/<>c::<RemoveDuplicates>b__11_1(System.Linq.IGrouping`2<TKey,AYellowpaper.SerializedCollections.SerializedKeyValuePair`2<TKey,TValue>>)
+// 0x0000000F AYellowpaper.SerializedCollections.SerializedKeyValuePair`2<TKey,TValue> AYellowpaper.SerializedCollections.DictionaryLookupTable`2/<>c::<RemoveDuplicates>b__11_2(System.Linq.IGrouping`2<TKey,AYellowpaper.SerializedCollections.SerializedKeyValuePair`2<TKey,TValue>>)
+// 0x00000010 System.Void AYellowpaper.SerializedCollections.IKeyable::RecalculateOccurences()
+// 0x00000011 System.Collections.Generic.IReadOnlyList`1<System.Int32> AYellowpaper.SerializedCollections.IKeyable::GetOccurences(System.Object)
+// 0x00000012 System.Collections.IEnumerable AYellowpaper.SerializedCollections.IKeyable::get_Keys()
+// 0x00000013 System.Void AYellowpaper.SerializedCollections.IKeyable::AddKey(System.Object)
+// 0x00000014 System.Void AYellowpaper.SerializedCollections.IKeyable::RemoveKey(System.Object)
+// 0x00000015 System.Void AYellowpaper.SerializedCollections.IKeyable::RemoveAt(System.Int32)
+// 0x00000016 System.Object AYellowpaper.SerializedCollections.IKeyable::GetKeyAt(System.Int32)
+// 0x00000017 System.Void AYellowpaper.SerializedCollections.IKeyable::RemoveDuplicates()
+// 0x00000018 System.Boolean AYellowpaper.SerializedCollections.SerializedCollectionsUtility::IsValidKey(System.Object)
+extern void SerializedCollectionsUtility_IsValidKey_mCD13AE41EA67D1B5479BB71A3646AAF375888E6A (void);
+// 0x00000019 System.Void AYellowpaper.SerializedCollections.SerializedDictionary`2::OnAfterDeserialize()
+// 0x0000001A System.Void AYellowpaper.SerializedCollections.SerializedDictionary`2::OnBeforeSerialize()
+// 0x0000001B System.Void AYellowpaper.SerializedCollections.SerializedDictionary`2::.ctor()
+// 0x0000001C System.Void AYellowpaper.SerializedCollections.SerializedDictionaryAttribute::.ctor(System.String,System.String)
+extern void SerializedDictionaryAttribute__ctor_mE5CDD8959982D1E1A772324806953E353E718E96 (void);
+// 0x0000001D System.Void AYellowpaper.SerializedCollections.SerializedKeyValuePair`2::.ctor(TKey,TValue)
+static Il2CppMethodPointer s_methodPointers[29] = 
+{
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	SerializedCollectionsUtility_IsValidKey_mCD13AE41EA67D1B5479BB71A3646AAF375888E6A,
+	NULL,
+	NULL,
+	NULL,
+	SerializedDictionaryAttribute__ctor_mE5CDD8959982D1E1A772324806953E353E718E96,
+	NULL,
+};
+static const int32_t s_InvokerIndices[29] = 
+{
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	7767,
+	0,
+	0,
+	0,
+	2020,
+	0,
+};
+static const Il2CppTokenRangePair s_rgctxIndices[3] = 
+{
+	{ 0x02000002, { 0, 32 } },
+	{ 0x02000003, { 32, 7 } },
+	{ 0x02000006, { 39, 22 } },
+};
+extern const uint32_t g_rgctx_Dictionary_2_tA095D7EC59CDD06587B0F4BC7AFED6E7A4FDAAE9;
+extern const uint32_t g_rgctx_Dictionary_2_get_Keys_mF9464C7C39B14A3433AE9C3C2BF15C30D3BCE4A2;
+extern const uint32_t g_rgctx_Dictionary_2_tA98E9A3EC883333A1DB118F3BCFA18D8BB1CE942;
+extern const uint32_t g_rgctx_Dictionary_2__ctor_m43C1F3BF50A8795FE54054DE9E9C7CA4F8173B0A;
+extern const uint32_t g_rgctx_TKey_tBFFEB40F1467D3FA48DDA3A4AD0A7233BC89A6EC;
+extern const uint32_t g_rgctx_Dictionary_2_TryGetValue_mCDA55F3262A15B14A3865D79246EB83BDFE3F3BA;
+extern const uint32_t g_rgctx_DictionaryLookupTable_2_tD30AB9A8CF8645361DC9E766751FFAA6843426E2;
+extern const uint32_t g_rgctx_Dictionary_2_Clear_mD32ABFB35EBDE0C70A66F9ACF39296841BFC1906;
+extern const uint32_t g_rgctx_List_1_t7E2611BE71B86F7723ACD2FD84C0551DECAB2AFE;
+extern const uint32_t g_rgctx_List_1_get_Count_m62886BE903BA9437C952A33C0B719058DE871E82;
+extern const uint32_t g_rgctx_List_1_get_Item_mB91418DD8D5A73091ABC937A327547B116B698B1;
+extern const uint32_t g_rgctx_Dictionary_2_ContainsKey_m0D13D996D6FE28DB7936CC3A65F77CB3089D0295;
+extern const uint32_t g_rgctx_Dictionary_2_Add_m65C67DD80B5C96670EC7129064F48E5981E3A5A5;
+extern const uint32_t g_rgctx_Dictionary_2_get_Item_m7242423F25F6C330E2CA04731E6F83EE46BFFFD3;
+extern const Il2CppRGCTXConstrainedData g_rgctx_TKey_tBFFEB40F1467D3FA48DDA3A4AD0A7233BC89A6EC_Object_Equals_m07105C4585D3FE204F2A80D58523D001DC43F63B;
+extern const uint32_t g_rgctx_List_1_RemoveAt_m297296B0230C64480A6AE15B57BFA14347EA8078;
+extern const uint32_t g_rgctx_SerializedKeyValuePair_2_tD643D710E10FFE08EF09A2843AFD2F575B9D625A;
+extern const uint32_t g_rgctx_U3CU3Ec_t6B8AB2FD8125640CE310592AEAD246F87B5B7CC0;
+extern const uint32_t g_rgctx_U3CU3Ec_U3CRemoveDuplicatesU3Eb__11_0_mEEBF81C34B2FBE1A099730938BE9D7286BDF6E67;
+extern const uint32_t g_rgctx_Func_2_t05BC83B52BB4245A279C0645647725F681072FDF;
+extern const uint32_t g_rgctx_Func_2__ctor_m1F76479B63BB15680847CD8E58AE1DDD7CA4DCE3;
+extern const uint32_t g_rgctx_Enumerable_GroupBy_TisSerializedKeyValuePair_2_tD643D710E10FFE08EF09A2843AFD2F575B9D625A_TisTKey_tBFFEB40F1467D3FA48DDA3A4AD0A7233BC89A6EC_m7F263C482D1C19918103EC09C1BB1C54BCBAF33E;
+extern const uint32_t g_rgctx_U3CU3Ec_U3CRemoveDuplicatesU3Eb__11_1_m8F7E03705AD6FE18A3DE5E0140972D658ED6B186;
+extern const uint32_t g_rgctx_Func_2_tB5C16112099B7E661C0ADDC41101A0CD56118FCA;
+extern const uint32_t g_rgctx_Func_2__ctor_mA9D2B9531A20870E9778AFE430952F00280885B5;
+extern const uint32_t g_rgctx_Enumerable_Where_TisIGrouping_2_tE4B4843BFC21B1D7AE59B66266CC74A6F9C246A1_m54C0E85E7EA231F41B3A45D3EB2A3AAEE33D44CD;
+extern const uint32_t g_rgctx_U3CU3Ec_U3CRemoveDuplicatesU3Eb__11_2_mB636972AD1D0FCCCD9827420E7072508416FCF1F;
+extern const uint32_t g_rgctx_Func_2_t82678445A887247CD3CB15D2F8DE4D2D2428BD9F;
+extern const uint32_t g_rgctx_Func_2__ctor_mDD0B3FD0DA8FD81A52870554EDA89E18359FA3F6;
+extern const uint32_t g_rgctx_Enumerable_Select_TisIGrouping_2_tE4B4843BFC21B1D7AE59B66266CC74A6F9C246A1_TisSerializedKeyValuePair_2_tD643D710E10FFE08EF09A2843AFD2F575B9D625A_mA58EBD3450E5A0D49F7A5D1F41619378D7441D6C;
+extern const uint32_t g_rgctx_Enumerable_ToList_TisSerializedKeyValuePair_2_tD643D710E10FFE08EF09A2843AFD2F575B9D625A_mD4281B6056173CD529AEFEEA2B3D511CA26E42C6;
+extern const uint32_t g_rgctx_List_1_Add_m17B65EE59FDB405037A8894851DE47F175900E00;
+extern const uint32_t g_rgctx_U3CU3Ec_t3AA9C4C9AB50565D86A5715213F94C0984125C01;
+extern const uint32_t g_rgctx_U3CU3Ec__ctor_m6B9DF355A7C2A9375930DA9FE31B93DDEE17458F;
+extern const uint32_t g_rgctx_U3CU3Ec_t3AA9C4C9AB50565D86A5715213F94C0984125C01;
+extern const uint32_t g_rgctx_IGrouping_2_t45A5845570006D71911CD7ECD38C28B238DAD6FE;
+extern const uint32_t g_rgctx_IGrouping_2_get_Key_mA6ACA6FAB8CBCD5064A9F1F6F887CE889D71209D;
+extern const uint32_t g_rgctx_TKey_t80B7393E94E22E500BC260E260779AB2308C193F;
+extern const uint32_t g_rgctx_Enumerable_First_TisSerializedKeyValuePair_2_tF4529F0ACEBA63086DC3B34F52EFF4C58C62D598_m176D6C11CF1255974DD98E1723663ADA8F4A75AE;
+extern const uint32_t g_rgctx_Dictionary_2_Clear_mA3E79853ED864C87CAA3ABF4DDB01E6F6D58E28B;
+extern const uint32_t g_rgctx_List_1_tAD6213F55A8206CC57120ED4C55A5F76505A8484;
+extern const uint32_t g_rgctx_List_1_GetEnumerator_mC6ABE745EE6FD5B2727ED2237E1E36A222ABF2CA;
+extern const uint32_t g_rgctx_Enumerator_get_Current_m724F0E80917E2D6B43579332AB4362246A4C6C42;
+extern const uint32_t g_rgctx_Dictionary_2_Add_mE7AD8CDCC59B7070CA694E224A113AE381D59CB8;
+extern const uint32_t g_rgctx_Enumerator_MoveNext_m13139214B82B8A9D6712B342D655EC36BCD02B94;
+extern const uint32_t g_rgctx_Enumerator_t62818AB6CB34BA2FD442D2B213A9C508F38E068D;
+extern const Il2CppRGCTXConstrainedData g_rgctx_Enumerator_t62818AB6CB34BA2FD442D2B213A9C508F38E068D_IDisposable_Dispose_m3C902735BE731EE30AC1185E7AEF6ACE7A9D9CC7;
+extern const uint32_t g_rgctx_List_1_Clear_mAE7B82B59FAB894CF86D21D44C75336004728D56;
+extern const uint32_t g_rgctx_Dictionary_2_GetEnumerator_mB483F565563CDADA4BD5EFD311AD2974C4A9E2D4;
+extern const uint32_t g_rgctx_Enumerator_get_Current_m2F98CDA89AEE1EA951F397C39E7745F9C2C1C796;
+extern const uint32_t g_rgctx_KeyValuePair_2_get_Key_mFB87343681F6B9A97FCD5E015608923B8C052E3D;
+extern const uint32_t g_rgctx_KeyValuePair_2_get_Value_m515A826F2959C4A64577B7EDAD2D58A228DAA9A3;
+extern const uint32_t g_rgctx_SerializedKeyValuePair_2_t48116C91C6CA5D9E73BD9BF16B9083D6F824D02E;
+extern const uint32_t g_rgctx_SerializedKeyValuePair_2__ctor_m2063A372DE47ED129639D0065ED155DFE4A42696;
+extern const uint32_t g_rgctx_List_1_Add_m714FE162A8C395303B05BDEB7006E764507B0D2C;
+extern const uint32_t g_rgctx_Enumerator_MoveNext_m85BC1BEF6E0AF16EA945D3AD618A82F61682FF8A;
+extern const uint32_t g_rgctx_Enumerator_tDDD2D3C4E7880394712261986788040A08B6F7AB;
+extern const Il2CppRGCTXConstrainedData g_rgctx_Enumerator_tDDD2D3C4E7880394712261986788040A08B6F7AB_IDisposable_Dispose_m3C902735BE731EE30AC1185E7AEF6ACE7A9D9CC7;
+extern const uint32_t g_rgctx_List_1__ctor_m27646A72143F173C680ED5B08DA2F4EB60215F97;
+extern const uint32_t g_rgctx_Dictionary_2__ctor_m4C76C30F282DBC0F06A6B354DE3C364964917829;
+extern const uint32_t g_rgctx_Dictionary_2_tFD4A430084E75639DBA14A2BCAAE4E78CF4A1F35;
+static const Il2CppRGCTXDefinition s_rgctxValues[61] = 
+{
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_Dictionary_2_tA095D7EC59CDD06587B0F4BC7AFED6E7A4FDAAE9 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Dictionary_2_get_Keys_mF9464C7C39B14A3433AE9C3C2BF15C30D3BCE4A2 },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_Dictionary_2_tA98E9A3EC883333A1DB118F3BCFA18D8BB1CE942 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Dictionary_2__ctor_m43C1F3BF50A8795FE54054DE9E9C7CA4F8173B0A },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_TKey_tBFFEB40F1467D3FA48DDA3A4AD0A7233BC89A6EC },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Dictionary_2_TryGetValue_mCDA55F3262A15B14A3865D79246EB83BDFE3F3BA },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_DictionaryLookupTable_2_tD30AB9A8CF8645361DC9E766751FFAA6843426E2 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Dictionary_2_Clear_mD32ABFB35EBDE0C70A66F9ACF39296841BFC1906 },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_List_1_t7E2611BE71B86F7723ACD2FD84C0551DECAB2AFE },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_List_1_get_Count_m62886BE903BA9437C952A33C0B719058DE871E82 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_List_1_get_Item_mB91418DD8D5A73091ABC937A327547B116B698B1 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Dictionary_2_ContainsKey_m0D13D996D6FE28DB7936CC3A65F77CB3089D0295 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Dictionary_2_Add_m65C67DD80B5C96670EC7129064F48E5981E3A5A5 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Dictionary_2_get_Item_m7242423F25F6C330E2CA04731E6F83EE46BFFFD3 },
+	{ (Il2CppRGCTXDataType)5, (const void *)&g_rgctx_TKey_tBFFEB40F1467D3FA48DDA3A4AD0A7233BC89A6EC_Object_Equals_m07105C4585D3FE204F2A80D58523D001DC43F63B },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_List_1_RemoveAt_m297296B0230C64480A6AE15B57BFA14347EA8078 },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_SerializedKeyValuePair_2_tD643D710E10FFE08EF09A2843AFD2F575B9D625A },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_U3CU3Ec_t6B8AB2FD8125640CE310592AEAD246F87B5B7CC0 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_U3CU3Ec_U3CRemoveDuplicatesU3Eb__11_0_mEEBF81C34B2FBE1A099730938BE9D7286BDF6E67 },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_Func_2_t05BC83B52BB4245A279C0645647725F681072FDF },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Func_2__ctor_m1F76479B63BB15680847CD8E58AE1DDD7CA4DCE3 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Enumerable_GroupBy_TisSerializedKeyValuePair_2_tD643D710E10FFE08EF09A2843AFD2F575B9D625A_TisTKey_tBFFEB40F1467D3FA48DDA3A4AD0A7233BC89A6EC_m7F263C482D1C19918103EC09C1BB1C54BCBAF33E },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_U3CU3Ec_U3CRemoveDuplicatesU3Eb__11_1_m8F7E03705AD6FE18A3DE5E0140972D658ED6B186 },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_Func_2_tB5C16112099B7E661C0ADDC41101A0CD56118FCA },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Func_2__ctor_mA9D2B9531A20870E9778AFE430952F00280885B5 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Enumerable_Where_TisIGrouping_2_tE4B4843BFC21B1D7AE59B66266CC74A6F9C246A1_m54C0E85E7EA231F41B3A45D3EB2A3AAEE33D44CD },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_U3CU3Ec_U3CRemoveDuplicatesU3Eb__11_2_mB636972AD1D0FCCCD9827420E7072508416FCF1F },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_Func_2_t82678445A887247CD3CB15D2F8DE4D2D2428BD9F },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Func_2__ctor_mDD0B3FD0DA8FD81A52870554EDA89E18359FA3F6 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Enumerable_Select_TisIGrouping_2_tE4B4843BFC21B1D7AE59B66266CC74A6F9C246A1_TisSerializedKeyValuePair_2_tD643D710E10FFE08EF09A2843AFD2F575B9D625A_mA58EBD3450E5A0D49F7A5D1F41619378D7441D6C },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Enumerable_ToList_TisSerializedKeyValuePair_2_tD643D710E10FFE08EF09A2843AFD2F575B9D625A_mD4281B6056173CD529AEFEEA2B3D511CA26E42C6 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_List_1_Add_m17B65EE59FDB405037A8894851DE47F175900E00 },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_U3CU3Ec_t3AA9C4C9AB50565D86A5715213F94C0984125C01 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_U3CU3Ec__ctor_m6B9DF355A7C2A9375930DA9FE31B93DDEE17458F },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_U3CU3Ec_t3AA9C4C9AB50565D86A5715213F94C0984125C01 },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_IGrouping_2_t45A5845570006D71911CD7ECD38C28B238DAD6FE },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_IGrouping_2_get_Key_mA6ACA6FAB8CBCD5064A9F1F6F887CE889D71209D },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_TKey_t80B7393E94E22E500BC260E260779AB2308C193F },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Enumerable_First_TisSerializedKeyValuePair_2_tF4529F0ACEBA63086DC3B34F52EFF4C58C62D598_m176D6C11CF1255974DD98E1723663ADA8F4A75AE },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Dictionary_2_Clear_mA3E79853ED864C87CAA3ABF4DDB01E6F6D58E28B },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_List_1_tAD6213F55A8206CC57120ED4C55A5F76505A8484 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_List_1_GetEnumerator_mC6ABE745EE6FD5B2727ED2237E1E36A222ABF2CA },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Enumerator_get_Current_m724F0E80917E2D6B43579332AB4362246A4C6C42 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Dictionary_2_Add_mE7AD8CDCC59B7070CA694E224A113AE381D59CB8 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Enumerator_MoveNext_m13139214B82B8A9D6712B342D655EC36BCD02B94 },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_Enumerator_t62818AB6CB34BA2FD442D2B213A9C508F38E068D },
+	{ (Il2CppRGCTXDataType)5, (const void *)&g_rgctx_Enumerator_t62818AB6CB34BA2FD442D2B213A9C508F38E068D_IDisposable_Dispose_m3C902735BE731EE30AC1185E7AEF6ACE7A9D9CC7 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_List_1_Clear_mAE7B82B59FAB894CF86D21D44C75336004728D56 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Dictionary_2_GetEnumerator_mB483F565563CDADA4BD5EFD311AD2974C4A9E2D4 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Enumerator_get_Current_m2F98CDA89AEE1EA951F397C39E7745F9C2C1C796 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_KeyValuePair_2_get_Key_mFB87343681F6B9A97FCD5E015608923B8C052E3D },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_KeyValuePair_2_get_Value_m515A826F2959C4A64577B7EDAD2D58A228DAA9A3 },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_SerializedKeyValuePair_2_t48116C91C6CA5D9E73BD9BF16B9083D6F824D02E },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_SerializedKeyValuePair_2__ctor_m2063A372DE47ED129639D0065ED155DFE4A42696 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_List_1_Add_m714FE162A8C395303B05BDEB7006E764507B0D2C },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Enumerator_MoveNext_m85BC1BEF6E0AF16EA945D3AD618A82F61682FF8A },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_Enumerator_tDDD2D3C4E7880394712261986788040A08B6F7AB },
+	{ (Il2CppRGCTXDataType)5, (const void *)&g_rgctx_Enumerator_tDDD2D3C4E7880394712261986788040A08B6F7AB_IDisposable_Dispose_m3C902735BE731EE30AC1185E7AEF6ACE7A9D9CC7 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_List_1__ctor_m27646A72143F173C680ED5B08DA2F4EB60215F97 },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_Dictionary_2__ctor_m4C76C30F282DBC0F06A6B354DE3C364964917829 },
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_Dictionary_2_tFD4A430084E75639DBA14A2BCAAE4E78CF4A1F35 },
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AYellowpaper_SerializedCollections_CodeGenModule;
+const Il2CppCodeGenModule g_AYellowpaper_SerializedCollections_CodeGenModule = 
+{
+	"AYellowpaper.SerializedCollections.dll",
+	29,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	3,
+	s_rgctxIndices,
+	61,
+	s_rgctxValues,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
---
+++ Slingshot_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/LeanLocalizationEditor.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch-cpp.hpp"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include <limits>
+
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// <Module>
+struct U3CModuleU3E_tF43F29423222291A6D74C2AB33EDEDD881D0DEA3 
+{
+};
+
+// <Module>
+
+// <Module>
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
